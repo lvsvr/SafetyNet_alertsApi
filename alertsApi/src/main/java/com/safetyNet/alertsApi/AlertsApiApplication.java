c1928b7c2eb35d44aa744b81fa4038c119821b08
w-boar <av.w.boar@gmail.com>
2021-08-15 11:51:36 +0200

    config : jacoco, log4j2, applications.properties

---
+++ alertsApi/src/main/java/com/safetyNet/alertsApi/AlertsApiApplication.java
@@ -0,0 +1,13 @@
+package com.safetyNet.alertsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertsApiApplication.class, args);
+	}
+
+}
