fef2b6fc3d35cb4c07d15130a8d00b94e34ef857
w-boar <av.w.boar@gmail.com>
2021-08-15 12:07:39 +0200

    config : jacoco, log4j2, application.properties

---
+++ alertsApi/src/test/java/com/safetyNet/alertsApi/AlertsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetyNet.alertsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
