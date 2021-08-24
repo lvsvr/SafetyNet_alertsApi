8bac123e2e62aa007d5d83e3c9bb1edf659f07e1
w-boar <av.w.boar@gmail.com>
2021-08-24 10:37:16 +0200

    Home + AgeCalculator

--- alertsApi/src/main/java/com/safetyNet/alertsApi/AlertsApiApplication.java
+++ alertsApi/src/main/java/com/safetyNet/alertsApi/AlertsApiApplication.java
@@ -5,16 +5,20 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.safetyNet.alertsApi.model.AgeCalculator;
+import com.safetyNet.alertsApi.model.MedicalRecord;
 
 @SpringBootApplication
 public class AlertsApiApplication {
 
 	private static final Logger logger = LogManager.getLogger(AlertsApiApplication.class);
-		
+
 	public static void main(String[] args) {
 		SpringApplication.run(AlertsApiApplication.class, args);
 		logger.info("app inititialized");
-		
+		AgeCalculator ac = new AgeCalculator();
+		MedicalRecord mr = new MedicalRecord("Bart", "Simpson", "26/08/1991", null, null);
+		logger.info(ac.calculateAge(mr.getBirthDate()));
 	}
-	
+
 }
